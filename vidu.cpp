5f73c61fc4349b83cb53c58a09900f84258b1b9b
Heuik6 <nguyentrunghieu14112006@gmail.com>
2023-02-05 16:47:06 +0700

    clearcode

--- vidu.cpp
+++ vidu.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int a;
-  cout << "Nhap 1 so nguyen a";
-  cin >> a;
-  cout << "boi so cua " << a << "voi 15 so dau tien la ";
-  for (int i = 1; i < 16; i++) {
-    cout << "\n" << a*i ;
-  }
+
+
+
+
   return 0;
 }
+
