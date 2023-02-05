e8beb030ca8a1f4f09c5eb5aace76fe8a4e17fc6
Heuik6 <nguyentrunghieu14112006@gmail.com>
2023-02-05 16:37:29 +0700

    clear

--- vidu.cpp
+++ vidu.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int x, y, z;
-  cout << "Nhap gia tri cua x: ";
-  cin >> x;
-  cout << "Nhap gia tri cua y: ";
-  cin >> y;
-  cout << "Nhap gia tri cua z: ";
-  cin >> z;
-  int max = x;
-  if (max < y) {
-    max = y;
-  } 
-  if (max < z) {
-    max = z;
+  int a;
+  cout << "Nhap 1 so nguyen a";
+  cin >> a;
+  cout << "boi so cua " << a << "voi 15 so dau tien la ";
+  for (int i = 1; i < 16; i++) {
+    cout << "\n" << a*i ;
   }
-  cout << "gia tri lon nhat la: " << max;
   return 0;
 }
